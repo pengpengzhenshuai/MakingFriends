be2b03294400739a34fe9888cc6d47f002af6320
pengpengzhenshuai <158934476@qq.com>
2019-05-15 19:08:15 +0800

    11

---
+++ MakingFriendsApi/src/main/java/com/makingfriends/api/ServerloginApplication.java
@@ -0,0 +1,13 @@
+package com.makingfriends.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerloginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerloginApplication.class, args);
+    }
+
+}
